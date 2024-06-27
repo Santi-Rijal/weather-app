00168134f7e6f84d96cc9245075841655b206cb3
Santi <100055564+Santi-Rijal@users.noreply.github.com>
2024-06-27 15:05:15 -0300

    Navbar done

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,43 @@
 "use client";
 
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+
+import style from "./page.module.scss";
+import Navbar from "@/components/Navbar/Navbar";
+
+const threshold = 0.002;
 
 const Home = () => {
   const [lat, setLat] = useState(null);
   const [long, setLong] = useState(null);
   const [error, setError] = useState(null);
   const [pending, setPending] = useState(true);
+  const [apiData, setApiData] = useState(null);
+
+  const getCurrentLocation = async (lat, long) => {
+    const options = {
+      method: "GET",
+      url: "https://yahoo-weather5.p.rapidapi.com/weather",
+      params: {
+        lat: lat,
+        long: long,
+        format: "json",
+        u: "c",
+      },
+      headers: {
+        "x-rapidapi-key": "5c8c14735bmsh3ddd43190f166dfp1d6a64jsn960d37cf18b5",
+        "x-rapidapi-host": "yahoo-weather5.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setApiData(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const getLocation = () => {
@@ -20,6 +51,13 @@ const Home = () => {
             setLat(latitude);
             setLong(longitude);
             setPending(false);
+
+            if (
+              Math.abs(latitude - lat) > threshold ||
+              Math.abs(longitude - long) > threshold
+            ) {
+              //getCurrentLocation(latitude, longitude);
+            }
           },
           (err) => {
             setError(err.message);
@@ -34,13 +72,9 @@ const Home = () => {
   }, []);
 
   return (
-    !pending && (
-      <div>
-        {error && <span>{error}</span>}
-        <span>latitude: {lat}</span>
-        <span>longitude: {long}</span>
-      </div>
-    )
+    <div className={style.homePageContainer}>
+      <Navbar />
+    </div>
   );
 };
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import style from "./navbar.module.scss";
+
+import { CiLocationOn, CiCirclePlus } from "react-icons/ci";
+
+const Navbar = () => {
+  const [selectedId, setSelectedId] = useState(0);
+
+  const newItemClicked = (id) => {
+    setSelectedId(id);
+  };
+
+  return (
+    <div className={style.navbarContainer}>
+      <section className={style.locationsContainer}>
+        <CiLocationOn
+          id={0}
+          className={`${style.currentLocationIcon} ${style.icons} ${
+            selectedId === 0 && style.selected
+          }`}
+          onClick={() => newItemClicked(0)}
+        />
+      </section>
+
+      <CiCirclePlus className={`${style.plusIcon} ${style.icons}`} />
+    </div>
+  );
+};
+
+export default Navbar;
